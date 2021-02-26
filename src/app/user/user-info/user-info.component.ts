3ee59734749f63619605967ff8a677a2d57a8a1e
Manav Chugh <manav.chugh@incedoinc.com>
2021-02-26 14:13:01 +0530

    Attribute Directive added

--- src/app/user/user-info/user-info.component.ts
+++ src/app/user/user-info/user-info.component.ts
@@ -8,4 +8,11 @@ import { User } from 'src/app/model/user';
 })
 export class UserInfoComponent {
 @Input() user :  User;
+
+myDynamicClasses= {'my-border' : true, 'my-feature' : false}
+
+onToggle(){
+  this.myDynamicClasses['my-border']=!this.myDynamicClasses['my-border'];
+  this.myDynamicClasses['my-feature']=!this.myDynamicClasses['my-feature'];
+}
 }
